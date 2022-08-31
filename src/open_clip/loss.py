60d839fb7848466bd180e8459144d67a9055a581
qianyc <qianyicheng@xiaohongshu.com>
2022-08-31 16:17:20 +0800

    .

--- src/open_clip/loss.py
+++ src/open_clip/loss.py
@@ -126,6 +126,7 @@ class ClipLoss(nn.Module):
         self.prev_num_logits = 0
         self.labels = {}
 
+    # strategy1: using batch indices to select augmetation features
     # def forward(self, image_features, text_features, logit_scale):
     #     device = image_features.device
     #     if self.world_size > 1:
@@ -173,48 +174,7 @@ class ClipLoss(nn.Module):
     #     return total_loss
 
 
-    def forward(self, image_features, text_features, logit_scale, image_aug_features=None, text_aug_features=None):
-        device = image_features.device
-        if self.world_size > 1:
-
-            all_image_features, all_text_features = gather_features(
-                image_features, text_features,
-                self.local_loss, self.gather_with_grad, self.rank, self.world_size, self.use_horovod)
-
-            all_image_aug_features, all_text_aug_features = gather_features(
-                image_aug_features, text_aug_features,
-                self.local_loss, self.gather_with_grad, self.rank, self.world_size, self.use_horovod)
-
-            if self.local_loss:
-                logits_per_image = logit_scale * image_features @ torch.cat([all_text_features, all_text_aug_features], dim=0).T
-                logits_per_text = logit_scale * text_features @ torch.cat([all_image_features, all_image_aug_features], dim=0).T
-            else:
-                logits_per_image = logit_scale * all_image_features @ torch.cat([all_text_features, all_text_aug_features], dim=0).T
-                logits_per_text = logit_scale * all_text_features @ torch.cat([all_image_features, all_image_aug_features], dim=0).T
-        else:
-            logits_per_image = logit_scale * image_features @ torch.cat([text_features, text_aug_features], dim=0).T
-            logits_per_text = logit_scale * text_features @ torch.cat([image_features, image_aug_features], dim=0).T
-
-        # calculated ground-truth and cache if enabled
-        pdb.set_trace()
-        num_logits = logits_per_image.shape[0]
-        if self.prev_num_logits != num_logits or device not in self.labels:
-            labels = torch.arange(num_logits, device=device, dtype=torch.long)
-            if self.world_size > 1 and self.local_loss:
-                labels = labels + num_logits * self.rank
-            if self.cache_labels:
-                self.labels[device] = labels
-                self.prev_num_logits = num_logits
-        else:
-            labels = self.labels[device]
-
-        total_loss = (
-            F.cross_entropy(logits_per_image, labels) +
-            F.cross_entropy(logits_per_text, labels)
-            ) / 2
-        return total_loss
-
-
+    # strategy2: split features and aug_features; set aug_features to None by default.
     # def forward(self, image_features, text_features, logit_scale, image_aug_features=None, text_aug_features=None):
     #     device = image_features.device
     #     if self.world_size >= 1:
@@ -261,4 +221,50 @@ class ClipLoss(nn.Module):
     #         F.cross_entropy(logits_per_text, labels)
     #         ) / 2
     #     return total_loss
+
+
+    # strategy3: split features and aug_features; set aug_features to blank tensor by default.
+    def forward(self, image_features, text_features, logit_scale, image_aug_features=None, text_aug_features=None):
+        device = image_features.device
+        if self.world_size > 1:
+
+            all_image_features, all_text_features = gather_features(
+                image_features, text_features,
+                self.local_loss, self.gather_with_grad, self.rank, self.world_size, self.use_horovod)
+
+            all_image_aug_features, all_text_aug_features = gather_features(
+                image_aug_features, text_aug_features,
+                self.local_loss, self.gather_with_grad, self.rank, self.world_size, self.use_horovod)
+
+            if self.local_loss:
+                logits_per_image = logit_scale * image_features @ torch.cat([all_text_features, all_text_aug_features], dim=0).T
+                logits_per_text = logit_scale * text_features @ torch.cat([all_image_features, all_image_aug_features], dim=0).T
+            else:
+                logits_per_image = logit_scale * all_image_features @ torch.cat([all_text_features, all_text_aug_features], dim=0).T
+                logits_per_text = logit_scale * all_text_features @ torch.cat([all_image_features, all_image_aug_features], dim=0).T
+        else:
+            logits_per_image = logit_scale * image_features @ torch.cat([text_features, text_aug_features], dim=0).T
+            logits_per_text = logit_scale * text_features @ torch.cat([image_features, image_aug_features], dim=0).T
+
+        # calculated ground-truth and cache if enabled
+        pdb.set_trace()
+        num_logits = logits_per_image.shape[0]
+        if self.prev_num_logits != num_logits or device not in self.labels:
+            labels = torch.arange(num_logits, device=device, dtype=torch.long)
+            if self.world_size > 1 and self.local_loss:
+                labels = labels + num_logits * self.rank
+            if self.cache_labels:
+                self.labels[device] = labels
+                self.prev_num_logits = num_logits
+        else:
+            labels = self.labels[device]
+
+        total_loss = (
+            F.cross_entropy(logits_per_image, labels) +
+            F.cross_entropy(logits_per_text, labels)
+            ) / 2
+        return total_loss
+
+
+
 
